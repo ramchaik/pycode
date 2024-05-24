24613670496b15454ed621d4895acc7a1c00d05a
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2024-05-24 00:34:47 -0300

    feat: create a sequential chain using 2 chains

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import getpass
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
 import argparse
 
@@ -24,15 +24,37 @@ code_prompt = PromptTemplate(
     template="Write a very short {language} function that will {task}",
     input_variables=["language", "task"]
 )
+test_prompt = PromptTemplate(
+    template="Write a test for the following {language} code: \n{code}",
+    input_variables=["language", "code"]
+)
 
 code_chain = LLMChain(
     llm=llm,
-    prompt=code_prompt
+    prompt=code_prompt,
+    output_key="code",
+)
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
 )
 
-result = code_chain({
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language", "task"],
+    output_variables=["test", "code"]
+) 
+
+result = chain({
     "language": args.language,
     "task": args.task
 })
 
-print(result['text'])
+# print(result)
+
+print("\n>>>> Generated code")
+print(result['code'])
+
+print("\n>>>> Generated test")
+print(result['test'])
