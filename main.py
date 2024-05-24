d1fcd4ca9f2d0deca95c1d8879ee3237be21b02a
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2024-05-24 00:05:52 -0300

    feat: add chain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import getpass
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Provide your Google API Key")
@@ -9,5 +11,19 @@ llm = ChatGoogleGenerativeAI(
    model="gemini-pro" 
 )
 
-result = llm.invoke("Write a very small poem")
-print(result.content)
+code_prompt = PromptTemplate(
+    template="Write a very short {language} function that will {task}",
+    input_variables=["language", "task"]
+)
+
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt
+)
+
+result = code_chain({
+    "language": "python",
+    "task": "return a list of numbers"
+})
+
+print(result['text'])
